cdf4f19d833e708756909324f404088754191946
loginwashere <login.was.here@gmail.com>
2016-12-25 18:31:45 +0200

    Add serving react app on subroutes from node

--- server.js
+++ server.js
@@ -399,6 +399,12 @@ app.delete('/api/transactions/:id', (req, res) => {
     .json();
 });
 
+app.get('*', (req, res) => {
+  res
+    .status(200)
+    .sendFile(`${__dirname}/build/index.html`);
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
