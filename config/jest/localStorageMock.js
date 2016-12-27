be0ec8c2ab64bc06f351f44ed29daf2373165e44
loginwashere <login.was.here@gmail.com>
2016-12-27 23:07:49 +0200

    Fix production build

---
+++ config/jest/localStorageMock.js
@@ -0,0 +1,17 @@
+if (!window.localStorage) {
+  const localStorageMock = (function() {
+    let store = {};
+    return {
+      getItem: function(key) {
+        return store[key] || null;
+      },
+      setItem: function(key, value) {
+        store[key] = value.toString();
+      },
+      clear: function() {
+        store = {};
+      }
+    };
+  })();
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+}
